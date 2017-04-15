b3715a3f042bfe7a4482afac047faf39710398eb
Aleksa Arsic <alexa.arsic@hotmail.com>
2017-04-15 19:54:54 +0200

    CISCO CTIOS Auto Caller

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace AutoCisco
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.monoFlat_ThemeContainer1 = new MonoFlat.MonoFlat_ThemeContainer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.monoFlat_Button2 = new MonoFlat.MonoFlat_Button();
+            this.cb44 = new System.Windows.Forms.CheckBox();
+            this.cb90 = new System.Windows.Forms.CheckBox();
+            this.monoFlat_Label1 = new MonoFlat.MonoFlat_Label();
+            this.monoFlat_ControlBox1 = new MonoFlat.MonoFlat_ControlBox();
+            this.monoFlat_Button1 = new MonoFlat.MonoFlat_Button();
+            this.monoFlat_Separator1 = new MonoFlat.MonoFlat_Separator();
+            this.monoFlat_HeaderLabel1 = new MonoFlat.MonoFlat_HeaderLabel();
+            this.txtNumber = new MonoFlat.MonoFlat_TextBox();
+            this.monoFlat_ThemeContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // monoFlat_ThemeContainer1
+            // 
+            this.monoFlat_ThemeContainer1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(41)))), ((int)(((byte)(50)))));
+            this.monoFlat_ThemeContainer1.Controls.Add(this.textBox1);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Button2);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.cb44);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.cb90);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Label1);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_ControlBox1);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Button1);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Separator1);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_HeaderLabel1);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.txtNumber);
+            this.monoFlat_ThemeContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.monoFlat_ThemeContainer1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.monoFlat_ThemeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.monoFlat_ThemeContainer1.Name = "monoFlat_ThemeContainer1";
+            this.monoFlat_ThemeContainer1.Padding = new System.Windows.Forms.Padding(10, 70, 10, 9);
+            this.monoFlat_ThemeContainer1.RoundCorners = true;
+            this.monoFlat_ThemeContainer1.Sizable = true;
+            this.monoFlat_ThemeContainer1.Size = new System.Drawing.Size(470, 220);
+            this.monoFlat_ThemeContainer1.SmartBounds = true;
+            this.monoFlat_ThemeContainer1.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.monoFlat_ThemeContainer1.TabIndex = 0;
+            this.monoFlat_ThemeContainer1.Text = "Cisco Caller";
+            this.monoFlat_ThemeContainer1.Enter += new System.EventHandler(this.monoFlat_ThemeContainer1_Enter);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(76)))), ((int)(((byte)(85)))));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(244, 185);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 27);
+            this.textBox1.TabIndex = 11;
+            // 
+            // monoFlat_Button2
+            // 
+            this.monoFlat_Button2.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_Button2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.monoFlat_Button2.Image = null;
+            this.monoFlat_Button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.monoFlat_Button2.Location = new System.Drawing.Point(350, 185);
+            this.monoFlat_Button2.Name = "monoFlat_Button2";
+            this.monoFlat_Button2.Size = new System.Drawing.Size(109, 25);
+            this.monoFlat_Button2.TabIndex = 10;
+            this.monoFlat_Button2.Text = "Find Store";
+            this.monoFlat_Button2.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.monoFlat_Button2.Click += new System.EventHandler(this.monoFlat_Button2_Click);
+            // 
+            // cb44
+            // 
+            this.cb44.AutoSize = true;
+            this.cb44.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.cb44.Location = new System.Drawing.Point(100, 185);
+            this.cb44.Name = "cb44";
+            this.cb44.Size = new System.Drawing.Size(71, 24);
+            this.cb44.TabIndex = 9;
+            this.cb44.Text = "00044";
+            this.cb44.UseVisualStyleBackColor = true;
+            this.cb44.CheckedChanged += new System.EventHandler(this.cb44_CheckedChanged);
+            // 
+            // cb90
+            // 
+            this.cb90.AutoSize = true;
+            this.cb90.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.cb90.Location = new System.Drawing.Point(13, 185);
+            this.cb90.Name = "cb90";
+            this.cb90.Size = new System.Drawing.Size(47, 24);
+            this.cb90.TabIndex = 8;
+            this.cb90.Text = "90";
+            this.cb90.UseVisualStyleBackColor = true;
+            this.cb90.CheckedChanged += new System.EventHandler(this.cb90_CheckedChanged);
+            // 
+            // monoFlat_Label1
+            // 
+            this.monoFlat_Label1.AutoSize = true;
+            this.monoFlat_Label1.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_Label1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.monoFlat_Label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(125)))), ((int)(((byte)(132)))));
+            this.monoFlat_Label1.Location = new System.Drawing.Point(9, 157);
+            this.monoFlat_Label1.Name = "monoFlat_Label1";
+            this.monoFlat_Label1.Size = new System.Drawing.Size(110, 20);
+            this.monoFlat_Label1.TabIndex = 7;
+            this.monoFlat_Label1.Text = "Select a prefix :";
+            // 
+            // monoFlat_ControlBox1
+            // 
+            this.monoFlat_ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.monoFlat_ControlBox1.EnableHoverHighlight = true;
+            this.monoFlat_ControlBox1.EnableMaximizeButton = false;
+            this.monoFlat_ControlBox1.EnableMinimizeButton = true;
+            this.monoFlat_ControlBox1.Location = new System.Drawing.Point(358, 15);
+            this.monoFlat_ControlBox1.Name = "monoFlat_ControlBox1";
+            this.monoFlat_ControlBox1.Size = new System.Drawing.Size(100, 25);
+            this.monoFlat_ControlBox1.TabIndex = 6;
+            this.monoFlat_ControlBox1.Text = "monoFlat_ControlBox1";
+            // 
+            // monoFlat_Button1
+            // 
+            this.monoFlat_Button1.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_Button1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.monoFlat_Button1.Image = null;
+            this.monoFlat_Button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.monoFlat_Button1.Location = new System.Drawing.Point(336, 108);
+            this.monoFlat_Button1.Name = "monoFlat_Button1";
+            this.monoFlat_Button1.Size = new System.Drawing.Size(93, 46);
+            this.monoFlat_Button1.TabIndex = 5;
+            this.monoFlat_Button1.Text = "Call";
+            this.monoFlat_Button1.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.monoFlat_Button1.Click += new System.EventHandler(this.monoFlat_Button1_Click);
+            // 
+            // monoFlat_Separator1
+            // 
+            this.monoFlat_Separator1.Location = new System.Drawing.Point(0, 172);
+            this.monoFlat_Separator1.Name = "monoFlat_Separator1";
+            this.monoFlat_Separator1.Size = new System.Drawing.Size(512, 10);
+            this.monoFlat_Separator1.TabIndex = 2;
+            this.monoFlat_Separator1.Text = "monoFlat_Separator1";
+            // 
+            // monoFlat_HeaderLabel1
+            // 
+            this.monoFlat_HeaderLabel1.AutoSize = true;
+            this.monoFlat_HeaderLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_HeaderLabel1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.monoFlat_HeaderLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.monoFlat_HeaderLabel1.Location = new System.Drawing.Point(13, 80);
+            this.monoFlat_HeaderLabel1.Name = "monoFlat_HeaderLabel1";
+            this.monoFlat_HeaderLabel1.Size = new System.Drawing.Size(158, 25);
+            this.monoFlat_HeaderLabel1.TabIndex = 1;
+            this.monoFlat_HeaderLabel1.Text = "Phone Number :";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.BackColor = System.Drawing.Color.Transparent;
+            this.txtNumber.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.txtNumber.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(183)))), ((int)(((byte)(191)))));
+            this.txtNumber.Image = null;
+            this.txtNumber.Location = new System.Drawing.Point(13, 108);
+            this.txtNumber.MaxLength = 32767;
+            this.txtNumber.Multiline = false;
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.ReadOnly = false;
+            this.txtNumber.Size = new System.Drawing.Size(296, 46);
+            this.txtNumber.TabIndex = 0;
+            this.txtNumber.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txtNumber.UseSystemPasswordChar = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 220);
+            this.Controls.Add(this.monoFlat_ThemeContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cisco Caller";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.monoFlat_ThemeContainer1.ResumeLayout(false);
+            this.monoFlat_ThemeContainer1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MonoFlat.MonoFlat_ThemeContainer monoFlat_ThemeContainer1;
+        private MonoFlat.MonoFlat_ControlBox monoFlat_ControlBox1;
+        private MonoFlat.MonoFlat_Button monoFlat_Button1;
+        private MonoFlat.MonoFlat_Separator monoFlat_Separator1;
+        private MonoFlat.MonoFlat_HeaderLabel monoFlat_HeaderLabel1;
+        private MonoFlat.MonoFlat_TextBox txtNumber;
+        private MonoFlat.MonoFlat_Label monoFlat_Label1;
+        private System.Windows.Forms.CheckBox cb44;
+        private System.Windows.Forms.CheckBox cb90;
+        private System.Windows.Forms.TextBox textBox1;
+        private MonoFlat.MonoFlat_Button monoFlat_Button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿/////////////////////////
+//Author: Aleksa Arsic//
+///////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoCisco
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void monoFlat_Button1_Click(object sender, EventArgs e)
+        {
+
+            string number = txtNumber.Text;
+            
+            if (cb44.Checked)
+            {
+                txtNumber.Text = "00044" + txtNumber.Text;
+                cb90.Checked = false;
+                //string number = txtNumber.Text;
+                number = number.Replace(" ", "");
+            }
+
+            if (cb90.Checked)
+            {
+                txtNumber.Text = "9" + txtNumber.Text;
+                cb44.Checked = false;
+               // string number = txtNumber.Text;
+                number = number.Replace(" ", "");
+            }
+            
+
+            Process[] processes = Process.GetProcessesByName("CTIOSSoftphone");
+            if (processes.Length > 0)
+            {
+                WindowHelper.BringProcessToFront(processes[0]);
+                SendKeys.SendWait("%(n)");
+                SendKeys.SendWait("{DOWN}");
+                SendKeys.SendWait("{DOWN}");
+                SendKeys.SendWait("{DOWN}");
+                SendKeys.SendWait("{DOWN}");
+                SendKeys.SendWait("{DOWN}");
+                SendKeys.SendWait("{DOWN}");
+                SendKeys.SendWait("{DOWN}");
+                SendKeys.SendWait("{DOWN}");
+                SendKeys.SendWait("{ENTER}");
+                System.Threading.Thread.Sleep(300);
+                SendKeys.SendWait("%(d)");
+                System.Threading.Thread.Sleep(600);
+                SendKeys.SendWait("{BKSP}");
+                System.Threading.Thread.Sleep(100);
+                SendKeys.SendWait("9" + number);
+                SendKeys.SendWait("{ENTER}");
+                WindowHelper.BringProcessToFront(processes[0]);
+                //SendKeys.SendWait("%(r)");
+
+            }
+            else
+            {
+                MessageBox.Show("Please launch CISCO Application first.", "CISCO Not running", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+           
+        }
+
+        private void monoFlat_ThemeContainer1_Enter(object sender, EventArgs e)
+        {
+            txtNumber.Text = Clipboard.GetText();
+
+        }
+
+        private void cb90_CheckedChanged(object sender)
+        {
+            
+        }
+
+        private void cb44_CheckedChanged(object sender)
+        {
+            
+        }
+
+        private void cb90_CheckedChanged(object sender, EventArgs e)
+        {
+            cb44.Checked = false;
+        }
+
+        private void cb44_CheckedChanged(object sender, EventArgs e)
+        {
+            cb90.Checked = false;
+        }
+
+        private void monoFlat_Button2_Click(object sender, EventArgs e)
+        {
+            string storenum = textBox1.Text;
+            System.Diagnostics.Process.Start("chrome.exe","https://my.morrisons.com/storefinder/" + storenum);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("If you are using Prefix 90 copy the whole store number. \n \n If you are using Prefix 00044 please DO NOT COPY only the inital 0!", "Instructions ",MessageBoxButtons.OK);
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            txtNumber.Text = Clipboard.GetText();
+        }
+    }
+}
